629915b34df7bc180b8a41442c5bf5c7b8e053e0
Horea Oros <horea.oros@gmail.com>
2023-11-22 22:18:41 +0200

    add P04, P05

--- AoC2021 P04/Program.cs
+++ AoC2021 P04/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,22 +34,126 @@ namespace AoC2021_P04
                 cards.Add(card);
             }
             sr.Close();
+
+            Console.WriteLine($"Part1: {part1(cards, nums)}");
+            Console.WriteLine($"Part2: {part2(cards, nums)}");
+            
+
+
+            
+        }
+
+        private static int part2(List<(int Value, bool Marked)[,]> cards, List<int> nums)
+        {
+            int lasCardToWin = 0, k = 0;
+            UnMarkAll(cards);
+            bool[] winners = new bool[cards.Count];
+            while(winners.Count(c => c == true) != cards.Count - 1)
+            {
+                Console.WriteLine($"Marking {nums[k]}...");
+                mark(cards, nums[k]);
+                checkWinners(cards, winners);
+                k++;
+            }
+            while (winners[lasCardToWin] == true)
+                lasCardToWin++;
+
+            Console.WriteLine($"Last card to win: {lasCardToWin + 1}...");
+
+            while (!isWinner(cards[lasCardToWin]))
+            {
+                Console.WriteLine($"Marking {nums[k]}...");
+                mark(cards[lasCardToWin], nums[k]);
+                k++;
+            }
+
+            int sum = SumUnmarked(cards[lasCardToWin]);
+            return  sum * nums[k - 1];
+        }
+
+        private static bool isWinner((int Value, bool Marked)[,] card)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                bool winning = true;
+                for (int j = 0; winning && j < 5; j++)
+                    winning = winning && card[i, j].Marked;
+                if (winning)
+                    return true;
+            }
+
+
+            for (int j = 0; j < 5; j++)
+            {
+                bool winning = true;
+                for (int i = 0; winning && i < 5; i++)
+                    winning = winning && card[i, j].Marked;
+                if (winning)
+                    return true;
+            }
+            return false;
+        }
+
+        private static void checkWinners(List<(int Value, bool Marked)[,]> cards, bool[] winners)
+        {
+            for (int k = 0; k < cards.Count; k++)
+            {
+                if (winners[k] == false)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        bool winning = true;
+                        for (int j = 0; winning && j < 5; j++)
+                            winning = winning && cards[k][i, j].Marked;
+                        if (winning)
+                            winners[k] = true;
+                    }
+
+
+                    for (int j = 0; j < 5; j++)
+                    {
+                        bool winning = true;
+                        for (int i = 0; winning && i < 5; i++)
+                            winning = winning && cards[k][i, j].Marked;
+                        if (winning)
+                            winners[k] = true;
+                    }
+                }
+            }
+        }
+
+        private static void UnMarkAll(List<(int Value, bool Marked)[,]> cards)
+        {
+            foreach (var c in cards)
+            {
+                for(int i = 0; i < 5; i++)
+                    for(int j = 0; j < 5; j++)
+                        c[i, j].Marked = false;
+            }
+        }
+
+        private static int SumUnmarked((int Value, bool Marked)[,] card)
+        {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    if (card[i, j].Marked == false)
+                        sum += card[i, j].Value;
+            return sum;     
+        }
+
+        private static int part1(List<(int Value, bool Marked)[,]> cards, List<int> nums)
+        {
             int winningCard;
             int k = 0;
-            while((winningCard = win(cards)) == -1)
+            while ((winningCard = win(cards)) == -1)
             {
                 mark(cards, nums[k]);
                 k++;
             }
 
-            int sum = 0;
-            for (int i = 0; i < 5; i++)
-                for (int j = 0; j < 5; j++)
-                    if (cards[winningCard][i, j].Marked == false)
-                        sum += cards[winningCard][i, j].Value;
-
-
-            Console.WriteLine($"Part 1: {sum * nums[k - 1]}");
+            
+            return SumUnmarked(cards[winningCard]) * nums[k - 1];
         }
 
         private static int win(List<(int Value, bool Marked)[,]> cards)
@@ -81,10 +185,14 @@ namespace AoC2021_P04
         private static void mark(List<(int Value, bool Marked)[,]> cards, int v)
         {
             for (int k = 0; k < cards.Count; k++)
-                for (int i = 0; i < 5; i++)
-                    for (int j = 0; j < 5; j++)
-                        if (cards[k][i, j].Value == v)
-                            cards[k][i, j].Marked = true;
+                mark(cards[k], v);
+        }
+        private static void mark((int Value, bool Marked)[,] card, int v)
+        {
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    if (card[i, j].Value == v)
+                        card[i, j].Marked = true;
         }
     }
 }
---
+++ AoC2021 P05/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AoC2021_P05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string line;
+            
+            Regex rx = new Regex("\\d+");
+            int x1, y1, x2, y2;
+            var lines = new List<(int x1, int y1, int x2, int y2)>();
+            while ((line = sr.ReadLine()) != null)
+            {
+                MatchCollection matches = rx.Matches(line);
+                x1 = int.Parse(matches[0].Value);
+                y1 = int.Parse(matches[1].Value);
+                x2 = int.Parse(matches[2].Value);
+                y2 = int.Parse(matches[3].Value);
+                lines.Add((x1, y1, x2, y2));
+            }
+            sr.Close();
+
+            Console.WriteLine($"Part 1: {part1(lines)}");
+            Console.WriteLine($"Part 2: {part2(lines)}");
+
+        }
+
+        private static object part2(List<(int x1, int y1, int x2, int y2)> lines)
+        {
+            int[,] diagram = new int[10000, 10000];
+            int result = 0;
+            int x1, y1, x2, y2;
+            foreach (var item in lines)
+            {
+                (x1, y1, x2, y2) = item;
+                if(x1 == x2 && y1 == y2)
+                        Console.WriteLine($"Hey {x1}, {y1}, {x2}, {y2}");
+                if (x1 == x2 || y1 == y2)
+                {
+                    if (x1 > x2)
+                        (x1, x2) = (x2, x1);
+                    if (y1 > y2)
+                        (y1, y2) = (y2, y1);
+                    if (x1 == x2) // vertical
+                        for (int i = y1; i <= y2; i++)
+                            diagram[i, x1]++;
+                    else if (y1 == y2) // horizontal
+                        for (int j = x1; j <= x2; j++)
+                            diagram[y1, j]++;
+                }
+                else // diagonal
+                {
+                    int dx = -Math.Sign(x1 - x2);
+                    int dy = -Math.Sign(y1 - y2);
+                    while (x1 != x2)
+                    {
+                        diagram[y1, x1]++;
+                        x1 += dx;
+                        y1 += dy;
+                    }
+                    diagram[y1, x1]++;
+                }
+            }
+            result = CountOverlap(diagram);
+            return result;
+        }
+
+        private static int part1(List<(int x1, int y1, int x2, int y2)> lines)
+        {
+            int[,] diagram = new int[10000, 10000];
+            int result = 0;
+            int x1, y1, x2, y2;
+            foreach (var item in lines)
+            {
+                (x1, y1, x2, y2) = item;
+                if (x1 > x2)
+                    (x1, x2) = (x2, x1);
+                if (y1 > y2)
+                    (y1, y2) = (y2, y1);
+                if (x1 == x2 || y1 == y2)
+                {
+                    if (x1 == x2)
+                        for (int i = y1; i <= y2; i++)
+                            diagram[i, x1]++;
+                    else if (y1 == y2)
+                        for (int j = x1; j <= x2; j++)
+                            diagram[y1, j]++;
+                }
+
+            }
+            result = CountOverlap(diagram);
+            return result;
+        }
+
+        private static int CountOverlap(int[,] diagram)
+        {
+            int result = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                    if (diagram[i, j] >= 2)
+                        result++;
+            }
+
+            return result;
+        }
+    }
+}
